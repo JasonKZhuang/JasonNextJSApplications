09680d5460bbb59fd327292e34a56c00057a12c0
JasonKZhuang <zhuangkaizhi@gmail.com>
2024-07-01 11:43:34 +1000

    update

--- interview-app/app/layout.tsx
+++ interview-app/app/layout.tsx
@@ -1,6 +1,6 @@
-import type {Metadata} from "next";
+import React from "react";
 import "./globals.css";
-import NavBar from "@/app/_components/navigation/nav";
+
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -15,7 +15,6 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
-                <NavBar/>
                 {children}
             </body>
         </html>
---
+++ jason-nextjs-basecamp/app/posts/clientQuery/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from 'react';
+import {QueryClient, QueryClientProvider, useQuery} from "react-query";
+
+const queryClient = new QueryClient();
+
+
+export default function UseReactQueryPage() {
+    return (
+        <QueryClientProvider client={queryClient}>
+           <Posts/>
+        </QueryClientProvider>
+    );
+}
+
+export function Posts() {
+
+    const {data, isPending, error} = useQuery({
+        queryKey: ['posts'],
+        queryFn: () => fetch("https://jsonplaceholder.typicode.com/posts", {}).then(r => r.json()),
+    });
+
+    if (isPending){
+        return <div>Loading...</div>
+    }
+
+    if (error){
+        throw new Error("Failed to fetch posts data");
+    }
+
+    console.log(data);
+
+    return (
+        <div className={"grid grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-3"}>
+            {data && data.map((post: any) => (
+                <div key={post.id}>
+                    <h3>{post.title}</h3>
+                    <p>{post.body}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+
